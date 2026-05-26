6224ba6d34464e2529c7ccfe6f84116ba79f9fb3
George Rus <georgerus@MacBook17.local>
2026-05-26 14:29:02 +0300

    Changed form color

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -13,7 +13,7 @@ function Input({ className, ...props }: React.ComponentProps<typeof TextInput> &
         ),
         Platform.select({
           web: cn(
-            'placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground outline-none transition-[color,box-shadow] md:text-sm',
+            'placeholder:text-muted-foreground selection:bg-primary outline-none transition-[color,box-shadow] md:text-sm',
             'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
             'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive'
           ),
